8429268a078709b83f707780777d7ed35263a9d3
davicorreajanuario30-a11y <davicorreajanuario30@gmail.com>
2026-08-31 21:51:10 -0300

    Add Firebase Database initialization to config

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,35 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
+import {
+  getDatabase
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAftyWN6Mgb4BGQgawGsdD5gJ-o_3eQrmM",
   authDomain: "lopes-digital-web.firebaseapp.com",
+
+  databaseURL:
+    "https://lopes-digital-web-default-rtdb.firebaseio.com",
+
   projectId: "lopes-digital-web",
-  storageBucket: "lopes-digital-web.firebasestorage.app",
+
+  storageBucket:
+    "lopes-digital-web.firebasestorage.app",
+
   messagingSenderId: "851495968730",
-  appId: "1:851495968730:web:2e56e1b84b12a26b9baae3"
+
+  appId:
+    "1:851495968730:web:2e56e1b84b12a26b9baae3"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
-export { app };
+const database = getDatabase(app);
+
+
+export {
+  app,
+  database
+};
